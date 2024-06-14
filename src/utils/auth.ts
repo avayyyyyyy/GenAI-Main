add5de9771527017cd3d683f61cf9b19796e2cfd
avayyyyyyy <jabhi465@gmail.com>
2024-06-14 19:57:23 +0530

    fix: 🐛 PROD Fixes

--- src/utils/auth.ts
+++ src/utils/auth.ts
@@ -1,10 +1,10 @@
 import NextAuth from "next-auth";
-import Github from "next-auth/providers/github";
+import Google from "next-auth/providers/google";
 import { PrismaAdapter } from "@auth/prisma-adapter";
 import prisma from "@/utils/db";
 export const { handlers, signIn, signOut, auth } = NextAuth({
   adapter: PrismaAdapter(prisma),
-  providers: [Github],
+  providers: [Google],
   session: {
     strategy: "jwt",
   },
