b92ac1ab1ae746e00dc57304f86057dd6f8a65a2
avayyyyyyy <jabhi465@gmail.com>
2024-06-19 12:56:03 +0530

    fix: 🐛 BUg fixes

--- src/utils/auth.ts
+++ src/utils/auth.ts
@@ -1,11 +1,15 @@
 import NextAuth from "next-auth";
 import Github from "next-auth/providers/github";
-import Google from "next-auth/providers/google";
 import { PrismaAdapter } from "@auth/prisma-adapter";
 import prisma from "@/utils/db";
 export const { handlers, signIn, signOut, auth } = NextAuth({
   adapter: PrismaAdapter(prisma),
-  providers: [Github, Google],
+  providers: [
+    Github({
+      clientId: process.env.AUTH_GITHUB_ID,
+      clientSecret: process.env.AUTH_GITHUB_SECRET!,
+    }),
+  ],
   session: {
     strategy: "jwt",
   },
